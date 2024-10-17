81b3282f8b8cc34d571108d3cbaedd4c7021020f
Rami-Aldahir <140156812+Rami-Aldahir@users.noreply.github.com>
2024-10-17 10:52:29 +0100

    Add files via upload

--- ControlPageVGS/extras.js
+++ ControlPageVGS/extras.js
@@ -17,6 +17,44 @@ powerCycleButton.addEventListener('click', () => {
     ws.send(JSON.stringify({"VGS_powercycle_admin": "1"}));
 });
 
+let lightMode1 = document.getElementById('mode_1');
+lightMode1.addEventListener('click', () => {
+    ws.send(JSON.stringify({"VGS_Lights": "1"}));
+})
+
+let lightMode2 = document.getElementById('mode_2');
+lightMode2.addEventListener('click', () => {
+    ws.send(JSON.stringify({"VGS_Lights": "2"}));
+})
+
+
+let lightMode3 = document.getElementById('mode_3');
+lightMode3.addEventListener('click', () => {
+    ws.send(JSON.stringify({"VGS_Lights": "3"}));
+})
+
+let lightMode4 = document.getElementById('mode_4');
+lightMode4.addEventListener('click', () => {
+    ws.send(JSON.stringify({"VGS_Lights": "4"}));
+})
+
+let lightMode5 = document.getElementById('mode_5');
+lightMode5.addEventListener('click', () => {
+    ws.send(JSON.stringify({"VGS_Lights": "5"}));
+})
+
+let lightMode6 = document.getElementById('mode_6');
+lightMode6.addEventListener('click', () => {
+    ws.send(JSON.stringify({"VGS_Lights": "6"}));
+})
+
+let lightMode7 = document.getElementById('mode_7');
+lightMode7.addEventListener('click', () => {
+    ws.send(JSON.stringify({"VGS_Lights": "7"}));
+})
+
+
+
 // WEBSOCKET OPEN/CLOSE/ERROR HANDLING
 ws.addEventListener('open', (event) => {
     console.log('Socket connection open');
@@ -42,6 +80,12 @@ ws.addEventListener('message', (message) => {
             const entranceStatus = document.getElementById('entrance_status');
             entranceStatus.value = data.VGS_PC11_is_on === null || data.VGS_PC11_is_on === undefined ? 0 : data.VGS_PC11_is_on;
         }
+
+        if (data.hasOwnProperty('VGS_Light_State')) {
+            const lightStatus = document.getElementById('lighting_state');
+            lightStatus.value = data.VGS_Light_State;
+        }
+        
     }
 });
 
